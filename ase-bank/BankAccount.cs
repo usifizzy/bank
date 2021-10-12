324bc2b07e9a197b53ec0d9a7d7ceb3ff8436b97
thalita <t.vergilio@leedsbeckett.ac.uk>
2021-10-12 15:27:00 +0100

    First commit

---
+++ ase-bank/BankAccount.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace ase_bank
+{
+    public class BankAccount
+    {
+        private string m_customerName;
+
+        private double m_balance;
+
+        private bool m_frozen = false;
+
+        private BankAccount()
+        {
+        }
+
+        public BankAccount(string customerName, double balance)
+        {
+            m_customerName = customerName;
+            m_balance = balance;
+        }
+
+        public string CustomerName
+        {
+            get { return m_customerName; }
+        }
+
+        public double Balance
+        {
+            get { return m_balance; }
+        }
+
+        /**
+        * This method subtracts the amount passed in from the account balance
+        * */
+        public void Debit(double amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        /**
+         * This method adds the amount passed in to the account balance
+         * */
+        public void Credit(double amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FreezeAccount()
+        {
+            m_frozen = true;
+        }
+
+        public void UnfreezeAccount()
+        {
+            m_frozen = false;
+        }
+
+        public static void Main()
+        {
+            BankAccount ba = new BankAccount("Mr. Bryan Walton", 11.99);
+            ba.Credit(5.77);
+            ba.Debit(11.22);
+            Console.WriteLine("Current balance is ${0}", ba.Balance);
+        }
+
+    }
+}
